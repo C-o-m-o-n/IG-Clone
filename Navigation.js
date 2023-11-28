5658322311ef978493f08ce179c59884933fc201
C-o-m-o-n <comon928@gmail.com>
2023-11-28 12:38:09 +0300

    bottomm nav complete

--- Navigation.js
+++ Navigation.js
@@ -1,8 +1,11 @@
-import { AntDesign, Entypo, FontAwesome5, Octicons, Ionicons, MaterialIcons } from '@expo/vector-icons';
+import { AntDesign, Entypo, FontAwesome, MaterialCommunityIcons,  FontAwesome5, Octicons, Ionicons, MaterialIcons } from '@expo/vector-icons';
 import HomeScreen from './Screens/HomeScreen';
 import ProfileScreen from './Screens/ProfileScreen';
 import StatusScreen from './Screens/StatusScreen';
-import LoginScreen from './Screens/LoginScreen';
+// import LoginScreen from './Screens/LoginScreen';
+import MediaScreen from './Screens/MediaScreen';
+import SearchScreen from './Screens/SearchScreen';
+
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
@@ -14,7 +17,7 @@ function MyTabs() {
 
 	const screenOptionStyle = {
 
-		tabBarStyle: { backgroundColor: "teal"}, 
+		tabBarStyle: { height: "80px"}, 
 };
 
 
@@ -22,40 +25,80 @@ function MyTabs() {
     <Tab.Navigator screenOptions={screenOptionStyle}>
       <Tab.Screen name="Home" component={HomeScreen}
 	   options={{
-          headerShown: false,
-        
+          headerShown: false, 
+			   tabBarShowLabel: false,
+
           tabBarIcon: ({ focused }) =>
             focused ? (
-				<MaterialIcons name="home-filled" size={26} color="red" />
+				<MaterialIcons name="home-filled" size={26} color="#ff8d00" />
             ) : (
               <Octicons name="home" size={20} color="black" />
 
             ),
         }}
-	  /> 
-      <Tab.Screen name="Status" component={StatusScreen}
-options={{
-          headerShown: false,
-          
+	  />
+  <Tab.Screen name="Search" component={SearchScreen}
+	   options={{
+          headerShown: false, 
+			   tabBarShowLabel: false,
+
           tabBarIcon: ({ focused }) =>
             focused ? (
-    	<Ionicons name="add-circle" size={24} color="red" />
+
+<Ionicons name="ios-search-circle" size={26} color="#ff8d00" />
+            ) : (
+<Ionicons name="ios-search" size={20} color="black" />
+
+            ),
+        }}
+	  />
+	        <Tab.Screen name="Status" component={StatusScreen}
+	  options={{
+          headerShown: false,
+          tabBarShowLabel: false,
+          tabBarIcon: ({ focused }) =>
+            focused ? (
+    	<Ionicons name="add-circle" size={24} color="#ff8d00" />
             ) : (
     	<Ionicons name="add-circle-outline" size={24} color="black" />
             ),
         }}
 
 	  />
+
+
+ <Tab.Screen name="Media" component={MediaScreen}
+	  options={{
+          headerShown: false,
+           tabBarShowLabel: false,
+
+          tabBarIcon: ({ focused }) =>
+            focused ? (
+
+				<MaterialCommunityIcons name="movie-filter" size={24} color="#ff8d00" />
+
+            ) : (
+				<MaterialCommunityIcons name="movie-filter-outline" size={24} color="black" />
+
+				
+            ),
+        }}
+
+	  />
+
+
 	  <Tab.Screen name="Profile" component={ProfileScreen}
 options={{
           headerShown: false,
-          
+           tabBarShowLabel: false,
+
           tabBarIcon: ({ focused }) =>
             focused ? (
-				<FontAwesome5 name="user-circle" size={24} color="red" />
+<FontAwesome name="user-circle-o" size={24} color="#ff8d00" />
+
             ) : (
 	<FontAwesome5 name="user-circle" size={24} color="black" />
-
+				
             ),
         }}
 
